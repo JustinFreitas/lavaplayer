c7d5448f8598a5abfbf6b66cb7457b07b10d6285
JustinFreitas <justin.s.freitas@me.com>
2025-06-23 15:47:24 -0600

    Incorporate new 2.2.4 changes.

--- main/build.gradle.kts
+++ main/build.gradle.kts
@@ -62,8 +62,6 @@ mavenPublishing {
 configurations.all {
     resolutionStrategy.dependencySubstitution {
         // https://mvnrepository.com/artifact/org.apache.groovy/groovy
-        //substitute(module("org.apache.groovy:groovy")).using(module("org.apache.groovy:groovy:4.0.26"))
-        // https://mvnrepository.com/artifact/org.slf4j/slf4j-api
-        //substitute(module("org.slf4j:slf4j-api:2.0.15")).using(module("org.slf4j:slf4j-api:2.0.16"))
+        substitute(module("org.apache.groovy:groovy")).using(module("org.apache.groovy:groovy:4.0.27"))
     }
 }
